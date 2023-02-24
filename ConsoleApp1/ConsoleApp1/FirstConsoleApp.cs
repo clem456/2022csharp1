66f43f1e66f7ccec18ac94d6a587ac28327468e2
Clement <35793@ma-web.nl>
2023-02-24 10:45:39 +0100

    new commits

---
+++ ConsoleApp1/ConsoleApp1/FirstConsoleApp.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 16;
+            bool finished = false;
+
+            float x = .1;
+            string gameName = "myGame";
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+    internal class gameEngine
+    {
+        gameEngineState = state;
+
+        internal bool Load(string file)
+        {
+            return true
+        }
+    }
+
+    internal enum gameEngineState
+    {
+
+    }
+
+}
