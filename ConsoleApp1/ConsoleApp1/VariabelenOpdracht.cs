cd261c19f4b8bf0c73640993ee2bc8a255f2e321
Clement <35793@ma-web.nl>
2023-02-24 12:08:05 +0100

    Week 2 variabelen Opdracht

---
+++ ConsoleApp1/ConsoleApp1/VariabelenOpdracht.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class VariabelenOpdracht
+    {
+        string programTitle = "reken Opdracht";
+        int programWindowPosx = 100;
+        int programWindowPosy = 100;
+        
+        static void Main(string[] args)
+        {
+            int xPos = 100;
+            float yPos;
+
+            yPos = .5f;
+
+            double money;
+            string playerName;
+            bool isSended;
+
+            playerName = "clement";
+            isSended = false;
+            money = 500000;
+
+            Console.WriteLine("player name is: " + playerName + System.Environment.NewLine + "bedrag is:" + money + System.Environment.NewLine + "is geld verstuurd : " + isSended);
+        }
+    }
+}
