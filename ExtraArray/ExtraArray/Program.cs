57d65954318e973ce7f5337edbdbe230c201e084
Clement <35793@ma-web.nl>
2023-03-24 09:49:38 +0100

    for loop opdracht

---
+++ ExtraArray/ExtraArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ExtraArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            int[] array = new int[] {10, 20, 30, 40, 50, 60};
+
+            int val = program.TelOp(array);
+
+            Console.WriteLine(val);
+        }
+
+        private int TelOp(int[] array)
+        {
+            int val = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                val += array[i];
+            }
+
+            return val;
+        }
+    }
+}
