365dcd129bc2f846db7083ecfde694c9d84ad45e
Clement <35793@ma-web.nl>
2023-04-05 15:29:20 +0200

    ClassesConstructor inhalen

---
+++ ClassesConstructor/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ClassesConstructor
+{
+    internal class Program
+    {
+        private class Rectangle
+        {
+            public int x;
+            public int y;
+            public int width;  
+            public int height;
+
+            internal Rectangle(int x, int y, int width, int height)
+            {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(5, 5, 100, 100);
+
+            Console.WriteLine($"x : {rect.x}, " +
+                $"y: {rect.y}, " +
+                $"width : {rect.width}, " +
+                $"height : {rect.height}");
+        }
+    }
+}
